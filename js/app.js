d9fff8cc63dab13abec753054f5754b1bfefebd5
AlekPawlowski <alekpawlowski21@gmail.com>
2017-07-14 10:02:34 +0200

    Wydaje mi sie ze wszystko jest dobrze bo u mnie działa

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("Hello Furry!");
+
+var Game = require("./game.js");
+
+var game = new Game();
+
+document.addEventListener('keydown', function(event){
+    game.turnFurry(event);
+});
+
+game.showFurry();
+
+game.showCoin();
+
+game.startGame();
